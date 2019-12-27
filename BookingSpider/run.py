385b5dd6e5e76d5a7c1cbdcbde061d2e6e4f2ff4
houkan <houkan@gridy.com>
2019-12-27 23:28:22 +0800

    run script

---
+++ BookingSpider/run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author   : Ken Hou
+# @Time     : 2019-12-27 23:27
+# @File     : run.py
+# @Software : PyCharm
+# @Project  : BookingSpider
+# @User     : kan
+# ----------------------------
+
+from scrapy.cmdline import execute
+execute(['scrapy','crawl','bookingcity'])
